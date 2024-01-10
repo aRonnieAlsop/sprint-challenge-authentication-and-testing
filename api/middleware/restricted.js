7ab2a12172a23e2bb4ed160b54cceffc31913c57
Arron Alsop <alsop.arron@gmail.com>
2024-01-09 18:52:45 -0800

    'restricted functioning'

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const token = req.headers.authorization
   if (!token) {
-    next({ message: 'token required' })
+    res.status(401).json({ message: 'token required' })
   } else {
     jwt.verify(token, 'secret', (err) => {
       if (err) {
-        next({ message: 'token invalid' })
+        res.status(401).json({ message: 'token invalid' })
       } else {
         next()
       }
